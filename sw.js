135b80925e46bee58ae8a694e1167d1920ff072d
whale-tracker-live <satalaitm5@gmail.com>
2026-01-20 15:26:20 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('install', e => e.waitUntil(caches.open('p1').then(c => c.addAll(['index.html']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
